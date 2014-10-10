2d47226aab620c34255c18fe603d84fcb57d5f9e
qDL7 <wybraniec@ymail.com>
2014-10-10 16:18:10 +0200

    Revert "Revert "laravel""
    This reverts commit c5ad5768911d32e3b90cd0e2b37f6a90f036ec3b.

---
+++ app/views/layouts/main.blade.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title> Teama7oes </title>
+		<link rel="stylesheet" href="{{ URL::asset('css/style.css')}}" />
+	</head>
+
+
+	<body>
+		<div class="logowanie">
+			@yield('content') 
+		</div>
+
+	</body>
+
+</html>
