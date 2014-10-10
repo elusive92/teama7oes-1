a01694b9c983e5b7f85c5367eeb5e3bbf068d070
Radoslaw W <radziu92@gmail.com>
2014-10-10 17:07:01 +0200

    Zmiana testowa

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('content')
-	Teama7oes - w budowie joł! </br>
+	Teama7oes - w budowie! </br>
 	<img src="http://www.zlok.net/blog/wp-content/uploads/under-construction-13.gif" alt="w budowie"> </br></br></br></br></br></br></br></br></br>
 
 	Zaloguj || Gość || Rejestracja
