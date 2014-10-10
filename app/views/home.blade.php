72d82e6228aa9909de78a55c7a40d0cf776439e1
qDL7 <wybraniec@ymail.com>
2014-10-10 16:09:41 +0200

    laravel

---
+++ app/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.main')
+
+@section('content')
+	Teama7oes - w budowie joł! </br>
+	<img src="http://www.zlok.net/blog/wp-content/uploads/under-construction-13.gif" alt="w budowie"> </br></br></br></br></br></br></br></br></br>
+
+	Zaloguj || Gość || Rejestracja
+@stop
