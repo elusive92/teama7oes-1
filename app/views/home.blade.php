f718bbd01d6548d65ac74f8b657e7759ce23a17f
Radoslaw W <radziu92@gmail.com>
2014-10-11 12:36:18 +0200

    Test Bootstrapa

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -1,8 +1,19 @@
 @extends('layouts.main')
 
 @section('content')
-	Teama7oes - w budowie! </br>
-	<img src="http://www.zlok.net/blog/wp-content/uploads/under-construction-13.gif" alt="w budowie"> </br></br></br></br></br></br></br></br></br>
-
-	Zaloguj || Gość || Rejestracja
+	<form role="form">
+		<div class="form-group">
+		    <label for="logowanie">Login</label>
+		    <input type="text" class="form-control" id="logowanie" placeholder="Login">
+		</div>
+		<div class="form-group">
+		    <label for="haslo">Password</label>
+		    <input type="password" class="form-control" id="haslo" placeholder="Password">
+		</div>
+		</br></br></br>
+		<p>
+			<button type="button" class="btn btn-primary btn-lg btn-block">Login</button>
+			<button type="button" class="btn btn-default btn-lg btn-block">Register</button>
+		</p>
+	</form>
 @stop
--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -2,6 +2,7 @@
 	<head>
 		<title> Teama7oes </title>
 		<link rel="stylesheet" href="{{ URL::asset('css/style.css')}}" />
+		<link rel="stylesheet" href="{{ URL::asset('css/bootstrap.css')}}" />
 	</head>
 
 
