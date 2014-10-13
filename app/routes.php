c79055f8e1c17cf586dfd57fb668b7066e5160a0
Radoslaw W <radziu92@gmail.com>
2014-10-13 20:43:58 +0200

    routes

--- app/routes.php
+++ app/routes.php
@@ -1,3 +1,8 @@
 <?php
 
-Route::get('/', array('as' => 'home', 'uses' => 'HomeController@getIndex'));
+/*Route::get('/', array('as' => 'home', 'uses' => 'HomeController@getIndex'));*/
+
+Route::get('/', array(
+	'as' => 'home', 
+	'uses' => 'HomeController@home'
+));
