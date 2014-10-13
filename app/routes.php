bee42e007711eb05f359247e53fc821906dee0b3
Koek71 <destniyz@gmail.com>
2014-10-13 10:50:07 +0200

    test
    zzzzzzzzzzzz

--- app/routes.php
+++ app/routes.php
@@ -1,3 +1,36 @@
 <?php
+//8===========================================D
+//zwykle stronki for all
+//8===========================================D
 
 Route::get('/', array('as' => 'home', 'uses' => 'HomeController@getIndex'));
+
+Route::get('tournaments', function()
+{
+    return 'Tu beda torunamenty';
+});
+
+Route::get('teams', function()
+{
+    return 'Cokolwiek';
+});
+
+Route::get('hailmarcin', function()
+{
+    return ' All hail wybraniec!';
+});
+
+//============================================
+// Admin======================================
+//============================================
+//Jak chcecie cos grupowego to napiszcie mi co i jak
+// ja glupi wiec bez modelu nic nie napisze:)))
+
+Route::group(array('prefix' => 'admin'), function()
+{
+    Route::get('/', function()
+    {
+        return View::make('powiedzmy głwona admina');
+    });
+});
+
