a53aee0211131253d442b54f29420eadec08d3cd
Radoslaw W <radziu92@gmail.com>
2014-10-15 23:39:02 +0200

    Layout
    +dopasowac kolory i rozmiary navbarow
    +dorobic ogolne tlo
    +i zrobic jakis glowny box gdzie bedzie zawartosc podstron

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -1,19 +1,8 @@
 @extends('layouts.main')
 
-@section('content')
-	<form role="form">
-		<div class="form-group">
-		    <label for="logowanie">Login</label>
-		    <input type="text" class="form-control" id="logowanie" placeholder="Login">
-		</div>
-		<div class="form-group">
-		    <label for="haslo">Password</label>
-		    <input type="password" class="form-control" id="haslo" placeholder="Password">
-		</div>
-		</br></br></br>
-		<p>
-			<button type="button" class="btn btn-primary btn-lg btn-block">Login</button>
-			<button type="button" class="btn btn-default btn-lg btn-block">Register</button>
-		</p>
-	</form>
+@section('glowna')
+	<h1> stronka z grami</h1>
+	</br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br>
+	</br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br>
+	<h2> ZAPRASZAMY <h2>
 @stop
