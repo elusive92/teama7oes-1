433b9355d9c4363c73735d613f8bc7b88ed74295
Radoslaw W <radziu92@gmail.com>
2014-10-18 18:43:28 +0200

    Górny navbar poprawiony

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -6,12 +6,13 @@
 		<link rel="stylesheet" href="{{ URL::asset('css/bootstrap.css')}}" />
 		<link rel="shortcut icon" type="image/x-icon" href="img/fav.png" />
     <style>
-      body {background-image: url('img/games/leagueoflegends.jpg')}
+      body {background-image: url('img/gradient.jpg')}
      /* potrzebna metoda, ktora w zaleznosci od wybranej gry ustawia background-image */
     </style>
 	</head>
 	<body>
-		<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+		<!--
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
 			<div class="navbar-header">
 				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                     <span class="sr-only">Toggle navigation</span>
@@ -19,11 +20,11 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-				<a class="navbar-brand" href="#"><img class="logo" src="img/nav_logo.png" alt="Tema7oes" height="25" width="160"></a>
+				<a class="navbar-brand" href="#"><img class="logo" src="#" alt="Tema7oes" height="25" width="160"></a>
             </div>
  			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
     				<ul class="nav navbar-nav pull-right nav_right">
-		                <!-- tak jak w navigation.blade.php dodac co widac przed a co po zalogowaniu-->
+		                
 	                	<li>
 	                        <a href="#">Sign In</a>
 	                    </li>
@@ -32,7 +33,12 @@
 	                    </li>
 					</ul>
  			</div>
-		</nav>
+		</nav> -->
+    <ul id="nav">
+        <img class="logo" src="img/nav_logo.png" alt="Tema7oes" height="25" width="160">
+        <li><a href="#">Sign In</a></li>
+        <li><a href="#">Sign Up</a></li>
+    </ul>
 
  		<div class="container">
         <nav class="navbar navbar-default" role="navigation">
