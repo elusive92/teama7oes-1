dec556048f58709b463ce197b30929ea3b5fe1a4
Radoslaw W <radziu92@gmail.com>
2014-10-24 00:23:15 +0200

    navbar na gorze

--- app/views/layout/navtop.blade.php
+++ app/views/layout/navtop.blade.php
@@ -1,11 +1,12 @@
 <script src="js/popup.js"></script>
 
 <ul id="nav">
-	<img class="logo" src="{{ URL::asset('/') }}img/nav_logo.png" alt="Tema7oes" height="25" width="160">
+	<a href="{{ URL::route('home')}}"><img class="logo" src="{{ URL::asset('/') }}img/nav_logo.png" alt="Tema7oes" height="30" width="178"></a>
 	@if(Auth::check())
-
+	
     <li><a href="{{ URL::route('account-sign-out') }}">Log out</a></li>
     <li><a href="{{ URL::route('account-profile') }}">{{ Auth::user()->username }}</a></li>
+    <li><img src="{{ Auth::user()->photo }}" width="30" height="30" /></li>
 	@else
     <li><a href="#" class="topopup2">Register</a></li>
     <li><a href="#" class="topopup">Login</a></li>
