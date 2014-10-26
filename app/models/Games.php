56faa133453fecc727f3ea173c74e5b75438832e
qDL7 <wybraniec@ymail.com>
2014-10-26 09:39:19 +0100

    chuj

--- app/models/Games.php
+++ app/models/Games.php
@@ -1,14 +1,9 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Piotrek
- * Date: 2014-10-21
- * Time: 07:53
- */
+
 class Games extends Eloquent {
 
     public $timestamps = false;
-    protected $table = "games";
+    protected $table = 'games';
     protected $fillable = array('gamename', 'descript', 'logo');
 
     public static  $rules = array(
