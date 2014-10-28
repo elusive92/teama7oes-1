cf2634868de66d860087d83d368e05eaf7a87ca5
Koek71 <destniyz@gmail.com>
2014-10-28 11:00:29 +0100

    wiii działa
    smiesznie :P

--- app/controllers/GalleryController.php
+++ app/controllers/GalleryController.php
@@ -2,6 +2,11 @@
 class GalleryController extends BaseController {
 
     public function getGallery(){
+        //-----------------------------------------
+        //$name = User::where('username', )
+        // tu sie to zautomatyzuje ale jeszce brak mi weny :D
+
+        //--------------------------------
         $photos = Gallery::where('id', 1)->get();
         return View::make('gallery.gallery')->with('photos', $photos);
     }
--- app/views/gallery/gallery.blade.php
+++ app/views/gallery/gallery.blade.php
@@ -2,12 +2,47 @@
 
 
 @section('content')
-<ul>
+<div id="myCarousel" class="carousel slide">
+
+
+
+    <ol class="carousel-indicators">
+    <?php $i=0 ?>
+    <?php $x=0 ?>
     @foreach($photos as $photo)
-        <li>
 
-        {{ HTML::image( 'media/gallery/'   ) }}
+        @if($i === 0)
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <?php $i++ ?>
+          @else
+            <li data-target="#myCarousel" data-slide-to=$i></li>
+            <?php $i++ ?>
+          @endif
+     @endforeach
+        </ol>
+        <!-- Carousel items -->
+        <div class="carousel-inner">
+       @foreach($photos as $photo)
 
-        </li>
-    @endforeach
-</ul>
+            @if($x=== 0)
+                <div class="active item">{{ HTML::image($photo->filename)}}</div>
+                <?php $x++ ?>
+            @else
+                   <div class="item">{{ HTML::image($photo->filename) }}</div>
+            @endif
+       @endforeach
+        </div>
+
+
+ <!-- Carousel nav -->
+    <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+  <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+</div>
+
+
+<script type="text/javascript">
+$(document).ready(function(){
+     $("#myCarousel").carousel();
+});
+</script>
+@stop
