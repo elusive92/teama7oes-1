d6e8503d323db87f2b3b89b32c303b08e697265b
Radoslaw W <radziu92@gmail.com>
2014-11-05 21:54:04 +0100

    nic nowego

--- app/views/account/profile.blade.php
+++ app/views/account/profile.blade.php
@@ -8,31 +8,28 @@
   <div id="profile">
 
     
-      <div id="userStats">
+      <div id="profilebox">
         <div class="profilepic">
           @if(Auth::user()->photo)
           <a href="#"><img src="{{ Auth::user()->photo }}" width="150" height="150" /></a>
           @else
           <a href="#"><img src="{{ URL::asset('/') }}img/default1.jpg" width="150" height="150" /></a>
           @endif
+          <div class="clear"><a href="{{ URL::route('account-editprofile')}}" class="btn btn-default">Edit profile</a></div>
         </div>
-        <div id="pright">
-        <a href="{{ URL::route('account-editprofile')}}" class="btn btn-default">Edit profile</a>
-        </div>
+        
         <div class="data">
-          <div  class="nickname"><h1>{{ Auth::user()->username }}</h1></div>
-          <h5>From: </h5>
-          <h5>Registered: </h5>
-          <h5>Games: </h5>
-          <h5>Teams: </h5>
+          <div class="nickname"><h1>{{ Auth::user()->username }}</h1></div>
+          <p>From: </p>
+          <p>Member since: </p>
+          <p>Games: </p>
+          <p>Teams: </p>
         </div>
         <div class="sep"></div>
 
         <h3>About Me:</h3>
         <p>{{ Auth::user()->about }}</p>
-      </div>
-      
-      
-    
+      </div>      
+  
   </div>
 @stop
