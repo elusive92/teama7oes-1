a75515d781d0bf3764bf0b55becd8d9511706a83
Radoslaw W <radziu92@gmail.com>
2014-11-10 01:03:15 +0100

    zz

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -8,12 +8,17 @@
 	
 	@if($news->count())
 		@foreach($news as $new)
-			<article>
-				<h3><a href="{{ URL::action('news-show', $new->id) }}">{{$new->title}} </a></h3>
-				<p> Created at {{ $new->created_at }} </p>
-				<p> {{Str::limit($new->descript, 130)}}</p>
-				<a href="{{ URL::action('news-show', $new->id) }}">Read moar </a>
-			</article>
+			<ul id="news_list">
+				<li class="clearfix">
+					<a href="">
+						<img src="{{ URL::asset('/') }}img/avatar_example.jpg" height="50" width="60">
+					</a>
+					<h3><a href="{{ URL::action('news-show', $new->id) }}">{{$new->title}} </a></h3>
+					<p> {{Str::limit($new->descript, 200)}}</p>
+					<a href="{{ URL::action('news-show', $new->id) }}">Read moar </a>
+					<p class="ptime"> Created at {{ $new->created_at }} </p>
+				</li>
+			</ul>
 		@endforeach
 	@endif
 
