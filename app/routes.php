5fcaa580a85048983c186d1052ddaeac57e7e372
Arkadiusz Pawluć <elusive92@gmail.com>
2014-11-18 20:17:14 +0100

    POWIESIC KOLKA!!!!

--- app/routes.php
+++ app/routes.php
@@ -173,7 +173,7 @@ Route::group(array('before' => 'auth'), function() {
     ));
 
     Route::get('/playerfriendlist', array(
-        'as' =>'playerFriendList',
+        'as' =>'friendlistPlayer',
         'uses' => 'FriendlistController@getPlayerFriendlist'
     ));
     
