8c62c51672fea6df64924214fd0be516315f9f3c
qDL7 <wybraniec@ymail.com>
2014-11-19 23:44:31 +0100

    include

--- app/views/layout/main.blade.php
+++ app/views/layout/main.blade.php
@@ -8,9 +8,9 @@
 		<link rel="stylesheet" href="{{ URL::asset('/') }}css/bootstrap.css" />
 		<link rel="shortcut icon" type="image/x-icon" href="{{ URL::asset('/') }}img/fav.png" />
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"> </script>
-    <script type="text/javascript" src="js/popup.js"></script>
-    <script src="js/jQuery.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="{{ URL::asset('/') }}js/popup.js"></script>
+    <script src="{{ URL::asset('/') }}js/jQuery.js"></script>
+    <script src="{{ URL::asset('/') }}js/bootstrap.min.js"></script>
     <style>
       body {background-image: url('{{ URL::asset('/') }}img/games/bg2.jpg')}
      /* potrzebna metoda, ktora w zaleznosci od wybranej gry ustawia background-image */
