5c39279b5605ebdaf6dde663ccee902327d490a6
Koek71 <destniyz@gmail.com>
2014-11-20 08:21:26 +0100

    z
    z

--- app/models/Blacklist.php
+++ app/models/Blacklist.php
@@ -9,4 +9,8 @@ class Blacklist extends Eloquent{
 
     protected $fillable = array('id','id_A','id_B','date');
 
+    public function user()
+    {
+        return $this->hasOne('User', 'id', 'id_B');
+    }
 }
