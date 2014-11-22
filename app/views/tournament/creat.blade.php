52b4160b6b0205e433449411b274d96360215aff
Arkadiusz Pawluć <elusive92@gmail.com>
2014-11-22 16:42:04 +0100

    .

--- app/views/tournament/creat.blade.php
+++ app/views/tournament/creat.blade.php
@@ -10,10 +10,16 @@
 
         <div class="form-group">
             <label for="tournamentname">Tournament name:</label> <input type="text" name="tournamentname" class="form-control" id="tournamentname" placeholder="Tournament name" {{ (Input::old('tournamentname')) ? 'value="' . e(Input::old('tournamentname')) . '"' : '' }}/>
-
         </div>
         @if($errors->has('tournamentname'))
         <p class='error'>{{ $errors->first('tournamentname') }}</p>
+        @endif
+
+        <div class="form-group">
+            <label for="descript">Full description here:</label> <input type="text" name="descript" class="form-control" id="descript" placeholder="description"/>
+        </div>
+        @if($errors->has('descript'))
+        <p class='error'>{{ $errors->first('descript') }}</p>
         @endif
 
         <input type="submit" value="Create" class="btn btn-default"/>
