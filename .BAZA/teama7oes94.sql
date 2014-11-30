3061a8914135d04e844418c9c10a1fc18db44bb7
Arkadiusz Pawluć <elusive92@gmail.com>
2014-11-30 15:48:09 +0100

    baza

--- .BAZA/teama7oes94.sql
+++ .BAZA/teama7oes94.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Czas generowania: 30 Lis 2014, 15:46
+-- Czas generowania: 22 Lis 2014, 16:23
 -- Wersja serwera: 5.6.20
 -- Wersja PHP: 5.5.15
 
@@ -252,7 +252,7 @@ CREATE TABLE IF NOT EXISTS `news` (
   `draft` tinyint(1) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=13 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=14 ;
 
 --
 -- RELACJE TABELI `news`:
@@ -270,7 +270,8 @@ INSERT INTO `news` (`id`, `game_id`, `title`, `descript`, `photo`, `draft`, `cre
 (6, 1, 'Specjal w czolgach', 'Pięciokrotnie więcej doświadczenia za pierwsze zwycięstwo dnia\r\n\r\nDoświadczenie to najpotężniejsza broń!\r\n\r\n15% zniżki na wszystkie pojazdy standardowe VIII poziomu\r\n\r\nCzas wspiąć się w górę drzewa technologicznego i zdobyć duże czołgi!', NULL, 0, '2014-11-12 15:53:37', '2014-11-12 15:53:37'),
 (7, 1, 'Dalsze losy zwyciestw', '\r\n    Zdobądźcie 111 111 PD w dowolnej liczbie bitew.\r\n    Znajdźcie się pośród 11 graczy, którzy zdobyli najwięcej bazowego doświadczenia podczas bitwy w drużynie (premie za pierwsze zwycięstwo dnia, konto premium itd. nie będą brane pod uwagę).', NULL, 0, '2014-11-12 15:55:19', '2014-11-12 15:55:19'),
 (8, 1, 'Assasin Cread', 'W historyczną rzeczywistość wmieszana jest historia zmagań Assasynów z Templariuszami, i chociaż działalność stronnictw przekłada się na historyczne wydarzenia, to w dalszym ciągu Unity mogłoby służyć jako znakomita pomoc naukowa w nauce historii tego okresu - choćby dlatego, że za sprawą gry ta historia i występujące w niej postacie ożywają: król Francji i król żebraków, Markiz de Sade i Robespierre, Napoleon i Vidoq wkraczają na scenę i zostawiają swój ślad w historii głównego bohatera Arno Doriana.\r\n\r\nWspinając się na 25 puktów widokowych francuskiej stolicy odkrywamy mapę Paryża ostatniej dekady osiemnastego wieku. Lud Paryża gromadzi się na placach i oblega budynki aktualnie sprawujących władzę. Barykady na ulicach, rewolucyjne napisy i dostrzegalne wszędzie ślady zniszczeń nie pozostawiają wątpliwości, że znaleźliśmy się w ważnym dziejowo momencie - słowem, idealna sceografia dla dramatu historycznego w jakim przyszło nam brać udział. Nie zamierzam zdradzać elementów fabuły, bo przechodząc Assasin’s Creed: Unity można się poczuć jak podczas oglądania hollywoodzkiej epopei.', NULL, 0, '2014-11-12 16:57:57', '2014-11-12 16:57:57'),
-(12, 1, 'Oferta dnia w Gamersgate', 'Bus Simulator 2012 - 2.10£\r\n\r\nPo obniżonej cenie możecie również nabyć:\r\n\r\nDefenders of Ardania – 2€ - Steam\r\nDefenders of Ardania Collection – 2.00£ – Steam\r\nImpire – 2.99£ – Steam\r\nMagicka – 1.74£ – Steam\r\nMagicka Collection – 5.74£ – Steam\r\nMagicka – Four Pack – 7.49€ - Steam\r\nWarlock: Master of the Arcane Complete Collection – 4.75£ – Steam', NULL, 0, '2014-11-18 13:37:40', '2014-11-18 13:37:40');
+(12, 1, 'Oferta dnia w Gamersgate', 'Bus Simulator 2012 - 2.10£\r\n\r\nPo obniżonej cenie możecie również nabyć:\r\n\r\nDefenders of Ardania – 2€ - Steam\r\nDefenders of Ardania Collection – 2.00£ – Steam\r\nImpire – 2.99£ – Steam\r\nMagicka – 1.74£ – Steam\r\nMagicka Collection – 5.74£ – Steam\r\nMagicka – Four Pack – 7.49€ - Steam\r\nWarlock: Master of the Arcane Complete Collection – 4.75£ – Steam', NULL, 0, '2014-11-18 13:37:40', '2014-11-18 13:37:40'),
+(13, 1, 'Oferta dnia na Steamie &#8211; Sakura Spirit ', 'Sakura Spirit – 3.39€\r\n\r\nOferta ważna do środy do godziny 19:00.', NULL, 0, '2014-11-18 13:40:22', '2014-11-18 13:40:22');
 
 -- --------------------------------------------------------
 
@@ -325,7 +326,7 @@ CREATE TABLE IF NOT EXISTS `teaminvitations` (
   `team_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `date` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=6 ;
 
 --
 -- RELACJE TABELI `teaminvitations`:
@@ -334,6 +335,13 @@ CREATE TABLE IF NOT EXISTS `teaminvitations` (
 --   `user_id`
 --       `users` -> `id`
 --
+
+--
+-- Zrzut danych tabeli `teaminvitations`
+--
+
+INSERT INTO `teaminvitations` (`id`, `team_id`, `user_id`, `date`) VALUES
+(5, 6, 4, '2014-11-19 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -347,7 +355,7 @@ CREATE TABLE IF NOT EXISTS `teammembers` (
   `team_id` int(11) NOT NULL,
   `joindate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `leftdate` datetime DEFAULT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=8 ;
 
 --
 -- RELACJE TABELI `teammembers`:
@@ -362,7 +370,13 @@ CREATE TABLE IF NOT EXISTS `teammembers` (
 --
 
 INSERT INTO `teammembers` (`id`, `user_id`, `team_id`, `joindate`, `leftdate`) VALUES
-(1, 3, 1, '2014-11-30 13:31:38', NULL);
+(1, 1, 4, '2014-11-11 10:24:06', NULL),
+(2, 3, 5, '2014-11-11 18:08:28', '2014-11-11 19:09:59'),
+(3, 3, 6, '2014-11-11 18:10:28', NULL),
+(4, 4, 7, '2014-11-12 14:46:53', NULL),
+(5, 1, 6, '0000-00-00 00:00:00', NULL),
+(6, 5, 6, '2014-11-12 15:50:35', NULL),
+(7, 10, 8, '2014-11-18 12:41:05', NULL);
 
 -- --------------------------------------------------------
 
@@ -373,7 +387,7 @@ INSERT INTO `teammembers` (`id`, `user_id`, `team_id`, `joindate`, `leftdate`) V
 CREATE TABLE IF NOT EXISTS `teams` (
 `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `game_id` int(11) NOT NULL,
+  `game_id` int(11) DEFAULT NULL,
   `teamname` varchar(50) COLLATE utf8_polish_ci NOT NULL,
   `logo` varchar(20) COLLATE utf8_polish_ci DEFAULT NULL,
   `ranking` int(5) NOT NULL DEFAULT '1000',
@@ -382,7 +396,7 @@ CREATE TABLE IF NOT EXISTS `teams` (
   `status` int(1) NOT NULL DEFAULT '0',
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=9 ;
 
 --
 -- RELACJE TABELI `teams`:
@@ -397,7 +411,11 @@ CREATE TABLE IF NOT EXISTS `teams` (
 --
 
 INSERT INTO `teams` (`id`, `user_id`, `game_id`, `teamname`, `logo`, `ranking`, `win`, `lose`, `status`, `created_at`, `updated_at`) VALUES
-(1, 3, 2, 'Test1', NULL, 1000, 0, 0, 0, '2014-11-30 14:31:38', '2014-11-30 14:31:38');
+(4, 1, NULL, 'PokeCenter', NULL, 1000, 0, 0, 0, '2014-11-11 11:24:06', '2014-11-11 11:24:06'),
+(5, 3, NULL, 'RazemPonadKilo', NULL, 1000, 0, 0, 1, '2014-11-11 19:08:28', '2014-11-11 19:09:59'),
+(6, 3, NULL, 'RazemPonadKilo2', '6.jpg', 1000, 0, 0, 0, '2014-11-11 19:10:28', '2014-11-11 19:10:35'),
+(7, 4, NULL, 'World of Tanks', '7.jpg', 1000, 0, 0, 0, '2014-11-12 15:46:53', '2014-11-12 15:47:34'),
+(8, 10, NULL, 'AdministratorzyPower', NULL, 1000, 0, 0, 0, '2014-11-18 13:41:05', '2014-11-18 13:41:05');
 
 -- --------------------------------------------------------
 
@@ -433,23 +451,16 @@ CREATE TABLE IF NOT EXISTS `tournaments` (
   `numberofplayers` int(2) NOT NULL,
   `name` varchar(30) COLLATE utf8_polish_ci NOT NULL,
   `descript` varchar(255) COLLATE utf8_polish_ci NOT NULL,
-  `regdate` date NOT NULL,
-  `startdate` date NOT NULL,
+  `regdate` datetime NOT NULL,
+  `startdate` datetime NOT NULL,
   `status` int(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=1 ;
 
 --
 -- RELACJE TABELI `tournaments`:
 --   `game_id`
 --       `games` -> `id`
 --
-
---
--- Zrzut danych tabeli `tournaments`
---
-
-INSERT INTO `tournaments` (`id`, `game_id`, `numberofteams`, `numberofplayers`, `name`, `descript`, `regdate`, `startdate`, `status`) VALUES
-(1, 2, 32, 64, 'Big Bang 4', 'Rozgrywki w LoLa. Zmiazdz przeciwnikow', '2014-11-23', '2014-11-30', 0);
 
 -- --------------------------------------------------------
 
@@ -480,14 +491,14 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 INSERT INTO `users` (`id`, `email`, `username`, `password`, `password_temp`, `code`, `active`, `permissions`, `about`, `created_at`, `updated_at`, `photo`, `remember_token`, `comefrom`) VALUES
 (1, 'elusive9225@gmail.com', 'elusive92', '$2y$10$FbYsCManSOWJC/T9KreUHOMOWVIdIDRtJSPbPRKe2dvMv.rdOve5m', '', '', 1, 1, 'Lubie grac w lola', '2014-11-08 16:08:28', '2014-11-08 16:08:57', NULL, 'Syev56lwRXqLyBZA3e30cOVpHfUFYtvNPdaxWa1Lp4dWUBOCj9taMVCScteb', ''),
-(3, 'elusive92@gmail.com', 'elu', '$2y$10$rFEbfq9IDySqmxXIhM9lOuVVTboU9o7vBEJtVtNYFQixVJb9LrGpK', '', '', 1, 1, 'Mam ksywke radzio', '2014-11-11 11:59:37', '2014-11-11 12:00:03', NULL, 'ZtXgq4vTwLJhZ6SWk5hUipTJLkgyjcdHF4msMtBNu5VKLYRkTBV7XgGXsAh8', ''),
+(3, 'elusive92@gmail.com', 'elu', '$2y$10$rFEbfq9IDySqmxXIhM9lOuVVTboU9o7vBEJtVtNYFQixVJb9LrGpK', '', '', 1, 1, 'Mam ksywke radzio', '2014-11-11 11:59:37', '2014-11-11 12:00:03', NULL, 'V1jYZHXSxl9ZE3cNxa2s9ZBCi7YvIJmhn5J2ADoSssnr0nmeKOzjDCYW0L9g', ''),
 (4, 'el@gmail.com', 'arek', '$2y$10$ctWyqoJ5J7rNtfFjwodcgu8ESFTGc6Vb3zChhFTjhbg1kiW/kBuG6', '', '', 1, 0, NULL, '2014-11-12 15:45:07', '2014-11-12 15:45:07', NULL, 'kR9i6ecuL40PCDgSm3vH7GEG2uHKEq2IEArpRKthAW0ddEcJeBxZy3ZvrsAp', ''),
 (5, 'els@gmail.com', 'marcinnic', '$2y$10$eg6Sd1fXbgPovLVaLtdG6Ol2IqCgt3gewiOdxqzV62gnTG9Jv.C8C', '', '', 1, 0, 'Jestem czarny, bialy i lubie biale kotki!', '2014-11-12 15:45:37', '2014-11-12 15:45:37', NULL, '', ''),
-(6, 'test@gmail.com', 'test', '$2y$10$xCNH8A1cvkw4rxUZo0ncNeOzDMiMr7RybA2xJsBTwP7yWKHqN.ER2', '', '', 1, 0, 'Konto testowe1', '2014-11-18 13:28:08', '2014-11-18 13:28:08', NULL, 'BerWpowzoBB1tFFWfK1LvYZQfVqjW7BAO3MAbd5fbWha3MWT7rQ0fIhfWia1', ''),
+(6, 'test@gmail.com', 'test', '$2y$10$xCNH8A1cvkw4rxUZo0ncNeOzDMiMr7RybA2xJsBTwP7yWKHqN.ER2', '', '', 1, 0, 'Konto testowe1', '2014-11-18 13:28:08', '2014-11-18 13:28:08', NULL, 'WXbjUdYb3vaN7c333WkVEbboTR1Ab2OkdKzWvBpSxdrmgWpzcgZnYKlghfCa', ''),
 (7, 'test1@gmail.com', 'test1', '$2y$10$yuleFeQkJ/l67oQCa3QA5ebUEfXmYvSdKjLzFh7i/qZwJBbmm6phG', '', '81B0jHgUAvtuAKqZJBwxw5PUeZUKZKAJEQQ3u6Xiln0Uql5fAJGW9upMRk5l', 0, 0, NULL, '2014-11-18 13:31:54', '2014-11-18 13:31:54', NULL, '', ''),
 (8, 'test2@gmail.com', 'test2', '$2y$10$HD3z6HNHokg6vU3mZ6iisee5ex5R0PC.sfGFfv5gkRTvhOfgAXRcy', '', '', 1, 0, NULL, '2014-11-18 13:32:45', '2014-11-18 13:32:45', NULL, '', ''),
 (9, 'test3@gmail.com', 'test3', '$2y$10$dmr1HmvbI65N6HRlP2rCeOabvy/HOGjv/ua6BpHebNClS2mTDGgPC', '', '', 1, 0, NULL, '2014-11-18 13:33:07', '2014-11-18 13:33:07', NULL, '', ''),
-(10, 'admin1@gmail.com', 'admin1', '$2y$10$v4kQuOQlWodWOGev4oq53O8AZCi8eJz9AAV8Cm3MROyP.yqHWHhau', '', '', 1, 1, NULL, '2014-11-18 13:33:26', '2014-11-18 13:33:26', NULL, '9bQ7n6yKS1ABh5qEmOmjuPyQzEDufBu49ITQO88N9ABxEyjMb4pMoqcIXplI', '');
+(10, 'admin1@gmail.com', 'admin1', '$2y$10$v4kQuOQlWodWOGev4oq53O8AZCi8eJz9AAV8Cm3MROyP.yqHWHhau', '', '', 1, 1, NULL, '2014-11-18 13:33:26', '2014-11-18 13:33:26', NULL, 'LDQdHno0KWUCHRUXBZ8IGjAQfBxhlquIOslRwJKBpNbULI8hYyQZtc65egJf', '');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -643,7 +654,7 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 -- AUTO_INCREMENT dla tabeli `news`
 --
 ALTER TABLE `news`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
 --
 -- AUTO_INCREMENT dla tabeli `players`
 --
@@ -658,17 +669,17 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 -- AUTO_INCREMENT dla tabeli `teaminvitations`
 --
 ALTER TABLE `teaminvitations`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT dla tabeli `teammembers`
 --
 ALTER TABLE `teammembers`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT dla tabeli `teams`
 --
 ALTER TABLE `teams`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT dla tabeli `tournamentmembers`
 --
@@ -678,7 +689,7 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 -- AUTO_INCREMENT dla tabeli `tournaments`
 --
 ALTER TABLE `tournaments`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT dla tabeli `users`
 --
