22942f3147df0e968bccbfd7650ebba4740fe180
Arkadiusz Pawluć <elusive92@gmail.com>
2014-12-02 13:48:02 +0100

    teraz!

--- app/controllers/TournamentController.php
+++ app/controllers/TournamentController.php
@@ -26,7 +26,7 @@ class TournamentController extends BaseController {
                 ));
             }
             if($tournament){
-			/return Redirect::route('tournaments');
+			return Redirect::route('tournaments');
 		}
 	}
 
